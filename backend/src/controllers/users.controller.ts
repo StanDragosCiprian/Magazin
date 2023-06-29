33e4a0e730a4414759c8db5dddb981b53fec325f
DragosCiprianStan <dragosciprian.sta@ulbsibiu.ro>
2023-06-29 18:11:51 +0300

    users update

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -9,10 +9,32 @@ import {
   ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from 'src/use-cases/users/users.service';
-
 import { CreateUsersDto } from 'src/core/dto/users.dto';
 import { Users } from 'src/core/entities/users.entity';
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+  @Post('/send')
+  async create(@Body() createUserDto: CreateUsersDto): Promise<Users> {
+    return this.usersService.create(createUserDto);
+  }
+  @Get('/getAll')
+  async getAll(): Promise<Users[]> {
+    return await this.usersService.getAll();
+  }
+  @Get(':id')
+  async getOneById(@Param('id', ParseIntPipe) id: number): Promise<Users> {
+    return await this.usersService.getOneById(id);
+  }
+  @Patch(':id')
+  async updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() newUserDto: CreateUsersDto,
+  ): Promise<Users> {
+    return await this.usersService.update(id, newUserDto);
+  }
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number): Promise<number> {
+    return await this.usersService.delete(id);
+  }
 }
